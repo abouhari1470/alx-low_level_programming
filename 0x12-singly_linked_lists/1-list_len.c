bf5562689981d754ab0bd283b1fb0b96d7166fd1
abouhari1470 <harimenshiaboubacar@gmail.com>
2023-09-21 12:54:01 -0700

    f

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,20 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/* Define the structure for a node in the linked list */
-typedef struct list_s {
-    int data;
-    struct list_s *next;
-} list_t;
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
 
-/* Function to count the number of elements in the linked list */
-size_t list_len(const list_t *h) {
-    size_t count = 0;  // Initialize count to 0
-
-    // Traverse the linked list
-    while (h != NULL) {
-        count++;      // Increment count for each node
-        h = h->next;  // Move to the next node
-    }
-
-    return count;
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
